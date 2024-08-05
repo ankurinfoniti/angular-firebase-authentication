66e6c18cc1cc3625e0eaeb43c5636992739c89ff
ankurinfoniti <ankur.infoniti@gmail.com>
2024-08-05 12:03:15 +0530

    add validation to login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,8 +7,31 @@ import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  fb = inject(FormBuilder);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  email = this.loginForm.get('email');
+  password = this.loginForm.get('password');
+
+  login() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+
+    console.log('Valid form submitted', this.loginForm.value);
+  }
+}

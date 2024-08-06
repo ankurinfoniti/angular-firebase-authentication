a24584e49ce1957aa9ed3bafa0d6ed81736079b0
ankurinfoniti <ankur.infoniti@gmail.com>
2024-08-06 13:48:53 +0530

    add guards to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
+import {
+  AuthGuard,
+  canActivate,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
 
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 
 export const routes: Routes = [
   {
@@ -14,13 +23,16 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    ...canActivate(redirectLoggedInToHome),
   },
   {
     path: 'sign-up',
     component: SignUpComponent,
+    ...canActivate(redirectLoggedInToHome),
   },
   {
     path: 'home',
     component: HomeComponent,
+    ...canActivate(redirectUnauthorizedToLogin),
   },
 ];
